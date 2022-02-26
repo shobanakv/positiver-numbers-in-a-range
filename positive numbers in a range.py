d82ba5fc596e6d545d8cea6cc6d4d37a3574e43a
shobanakv <90678251+shobanakv@users.noreply.github.com>
2022-02-26 12:39:58 +0530

    Add files via upload

---
+++ positive numbers in a range.py
@@ -0,0 +1,16 @@
+# Print positive numbers in a list
+
+# input of list
+li=[]
+n=int(input("Enter size of list"))
+for i in range(0,n):
+    e=int(input("Enter element of list"))
+    li.append(e)
+    
+print("Positive numbers in",li,"are: ")
+
+#traversing
+for i in li:
+    #checking condition
+    if i >= 0:
+        print(i, end = " ")
